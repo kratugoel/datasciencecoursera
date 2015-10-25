00e0c5875051474c8bd9e6f1aeadb6f6d9b50fcf
Kratu Goel <kratugoel@gmail.com>
2015-10-26 03:17:42 +0530

    cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,76 @@
+# makeCacheMatrix is a function that returns a list of functions
+# Its puspose is to store a martix and a cached value of the inverse of the 
+# matrix. Contains the following functions:
+# * setMatrix      set the value of a matrix
+# * getMatrix      get the value of a matrix
+# * cacheInverse   get the cahced value (inverse of the matrix)
+# * getInverse     get the cahced value (inverse of the matrix)
+#
+# Notes:
+# This is a general code I have written as part of my answer.
+# An example of the code shall be after this.
+# makeCacheMatrix function
+
+makeCacheMatrix <- function(x = numeric()) {
+  
+  # holds the cached value or NULL if nothing is cached
+  # initially nothing is cached so set it to NULL
+  cache <- NULL
+  
+  # store a matrix
+  setMatrix <- function(newValue) {
+    x <<- newValue
+    # since the matrix is assigned a new value, flush the cache
+    cache <<- NULL
+  }
+  
+  # returns the stored matrix
+  getMatrix <- function() {
+    x
+  }
+  
+  # cache the given argument 
+  cacheInverse <- function(solve) {
+    cache <<- solve
+  }
+  
+  # get the cached value
+  getInverse <- function() {
+    cache
+  }
+  
+  # return a list. Each named element of the list is a function
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+}
+
+
+# The following function calculates the inverse of a "special" matrix created with 
+# makeCacheMatrix
+cacheSolve <- function(y, ...) {
+  # get the cached value
+  inverse <- y$getInverse()
+  # if a cached value exists return it
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  # otherwise get the matrix, caclulate the inverse and store it in
+  # the cache
+  data <- y$getMatrix()
+  inverse <- solve(data)
+  y$cacheInverse(inverse)
+  
+  # return the inverse
+  inverse
+}
+
+#Example code.
+
+a <- makeCacheMatrix();
+summary(a);
+# create a square matrix (reason `solve` only handles square matrices )
+a$setMatrix( matrix(c(1,2,12,13), nrow = 2, ncol = 2) );
+a$getMatrix();
+cacheSolve(a)
+# the 2nd time we run the function, we get the cached value
+cacheSolve(a)
